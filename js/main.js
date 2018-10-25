dd17990256f4abf123ad9dac142c1855e51e076c
Oscar Servellon <rockerolife66@gmail.com>
2018-10-24 18:28:45 -0700

    need styling

--- js/main.js
+++ js/main.js
@@ -1,87 +1,91 @@
 var zodiacs = [
 	{
-		name: "Aquarius",
+		name: "aquarius",
 		fortune: "Knowledge, Humanitarian, Serious, Insightful, Duplicitous",
 		image: "img/aquarius.jpg",
 		date: "January 20th- February 18th"
 	},
 	{
-		name: "Aries",
+		name: "aries",
 		fortune: "Active, Demanding, Determined, Effective, Ambitious",
 		image: "img/aries.png",
 		date: "March 21st- April 19th"
 	},
 	{
-		name: "Cancer",
+		name: "cancer",
 		fortune: "Emotion, Diplomatic, Intensity, Impulsive, Selective",
 		image: "img/cancer.jpg",
 		date: "June 21st- July 22th"
 	},
 	{
-		name: "Capricorn",
+		name: "capricorn",
 		fortune: "Determination, Dominance, Perservering, Practical, Willful",
 		image: "img/capricorn.jpg",
 		date: "Decemeber 22nd- January 19th"
 	},
 	{
-		name: "Gemini",
+		name: "gemini",
 		fortune: "Communication, Indecision, Inquisitive, Intelligent, Changeable",
 		image: "img/gemini.jpg",
 		date: "May 21st- June 20th"
 	},
 	{
-		name: "Leo",
+		name: "leo",
 		fortune: "Ruling, Warmth, Generosity, Faithful, Initiative",
 		image: "img/leo.jpg",
 		date: "July 23rd- August 22nd"
 	},
 	{
-		name: "Libra",
+		name: "libra",
 		fortune: "Balance, Justice, Truth, Beauty, Perfection",
 		image: "img/libra.jpg",
 		date: "Septemeber 23rd- October 22nd"
 	},
 	{
-		name: "Pisces",
+		name: "pisces",
 		fortune: "Fluctuation, Depth, Imagination, Reactive, Indecisive",
 		image: "img/pisces.jpg",
 		date: "February 19th- March 20th"
 	},
 	{
-		name: "Sagittarius",
+		name: "sagittarius",
 		fortune: "Philosophical, Motion, Experimentation, Optimism",
 		image: "img/sagittarius.jpg",
 		date: "November 22nd- December 21st"
 	},
 	{
-		name: "Scorpio",
+		name: "scorpio",
 		fortune: "Transient, Self-Willed, Purposeful, Unyielding",
 		image: "img/scorpio.jpg",
 		date: "October 23rd- November 21st"
 	},
 	{
-		name: "Taurus",
+		name: "taurus",
 		fortune: "	Security, Subtle strength, Appreciation, Instruction, Patience",
 		image: "img/taurus.jpg",
 		date: "April 20th- May 20th"
 	},
 	{
-		name: "Virgo",
+		name: "virgo",
 		fortune: "Analyzing, Practical, Reflective, Observation, Thoughtful",
 		image: "img/scorpio.jpg",
 		date: "August 23rd- September 22nd"
 	}
 ];
 
-var input = document.getElementById("zodiacSign");
+
 var myH1 = document.getElementById("signName");
 var myH6= document.getElementById("dates");
 var myH3 = document.getElementById("fortune");
 var myImage = document.getElementById("image");
 
 function getZodiac(){
+
 	for(var i= 0; i<= zodiacs.length; i++) {
-		if(input.value == zodiacs[i].name) {
+		// var input = document.getElementById("userInput")
+		var input = document.getElementById("userInput").value;
+
+		if(input.trim().toLowerCase() == zodiacs[i].name) {
 			// console.log("success!");
 			myH1.innerText = zodiacs[i].name;
 			myH6.innerText= zodiacs[i].date;
